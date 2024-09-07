3ea4f42606c92b20e1d85d51827db811152f0d71
ivanderson2066 <132177377+ivanderson2066@users.noreply.github.com>
2024-09-07 12:53:50 -0300

    atualização

--- services.py
+++ services.py
@@ -1,24 +1,29 @@
-
-import models 
+import models
 from models import EstoqueItem
 import config
 from config import db
-def adicionar_item(item: EstoqueItem):
-    db.collection("estoque").document(item.id).set(item.to_dict())
 
-def obter_item(id: str):
-    doc = db.collection("estoque").document(id).get()
+# Adicionar um item usando o email como ID
+def adicionar_item(item: EstoqueItem, email: str):
+    db.collection("users").document(email).collection("items").document(item.id).set(item.to_dict())
+
+# Obter um item específico
+def obter_item(id: str, email: str):
+    doc = db.collection("users").document(email).collection("items").document(id).get()
     if doc.exists:
         return EstoqueItem.from_dict(doc.to_dict())
     else:
         return None
 
-def atualizar_item(id: str, dados_atualizados: dict):
-    db.collection("estoque").document(id).update(dados_atualizados)
+# Atualizar um item existente
+def atualizar_item(id: str, dados_atualizados: dict, email: str):
+    db.collection("users").document(email).collection("items").document(id).update(dados_atualizados)
 
-def remover_item(id: str):
-    db.collection("estoque").document(id).delete()
+# Remover um item
+def remover_item(id: str, email: str):
+    db.collection("users").document(email).collection("items").document(id).delete()
 
-def listar_itens():
-    docs = db.collection("estoque").stream()
+# Listar todos os itens de um usuário
+def listar_itens(email: str):
+    docs = db.collection("users").document(email).collection("items").stream()
     return [EstoqueItem.from_dict(doc.to_dict()) for doc in docs]
